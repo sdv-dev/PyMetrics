27979debb03467f941e4a088e6a993df37ce1be7
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2025-02-28 13:19:18 +0100

    Add tasks.py to lint

--- tasks.py
+++ tasks.py
@@ -1,13 +1,17 @@
+"""Invoke task definitions."""
+
 from invoke import task
 
 
 @task
 def lint(c):
-    c.run("ruff check .")
-    c.run("ruff format --check --diff .")
+    """Run lint checks using ruff."""
+    c.run('ruff check .')
+    c.run('ruff format --check --diff .')
 
 
 @task
 def fix_lint(c):
-    c.run("ruff check --fix .")
-    c.run("ruff format .")
+    """Automatically fix lint issues using ruff."""
+    c.run('ruff check --fix .')
+    c.run('ruff format .')
