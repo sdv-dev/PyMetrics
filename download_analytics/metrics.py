29910ee30ab73f330424dd9269e752589a30fc93
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2025-03-05 23:52:37 +0100

    Update metrics.py

--- download_analytics/metrics.py
+++ download_analytics/metrics.py
@@ -35,17 +35,17 @@ def _historical_groupby(downloads, groupbys=None):
     if groupbys is None:
         groupbys = downloads.set_index('timestamp').columns
 
-    new_columns = []  # Collect grouped DataFrames here
-
+    new_columns = []
     for groupby in groupbys:
         grouped = downloads.groupby([year_month, groupby])
         grouped_sizes = grouped.size().unstack(-1)  # noqa: PD010
         if len(groupbys) > 1:
             grouped_sizes.columns = f"{groupby}='" + grouped_sizes.columns + "'"
-        new_columns.append(grouped_sizes.fillna(0))  # Store for later
+            
+        new_columns.append(grouped_sizes.fillna(0))
 
     if new_columns:
-        base = pd.concat([base] + new_columns, axis=1)  # Add all columns at once
+        base = pd.concat([base] + new_columns, axis=1)
 
     totals = base.sum()
     totals.name = 'total'
