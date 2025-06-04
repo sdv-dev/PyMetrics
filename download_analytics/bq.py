2613503e54db3b49d657b9fe748cd3981094609d
Gaurav Sheni <gvsheni@gmail.com>
2025-06-04 13:11:55 -0400

    Add gretel-client and mostlyai-mock  (#19)
    * wip
    
    * lint
    
    * fix start date
    
    * add project print
    
    * fix print
    
    * update message
    
    * update to use pyarrow dtypes
    
    * fix string
    
    * update to ubuntu-latest-largeA
    
    * update to ubuntu
    
    * fix engine
    
    * docstring
    
    * use category dtype
    
    * remove pyarrow
    
    * fix ns
    
    * lint
    
    * use pyarrow everywhere
    
    * remove pyarrow dtypes
    
    * add readme instructions
    
    * fix manual
    
    * cleanup
    
    * fix manual
    
    * fix manual
    
    * fix max_days
    
    * fix docs

--- download_analytics/bq.py
+++ download_analytics/bq.py
@@ -24,11 +24,19 @@ def _get_bq_client(credentials_file):
 
         LOGGER.info('Loading BigQuery credentials from BIGQUERY_CREDENTIALS envvar')
 
-    service_account_info = json.loads(credentials_contents)
-    credentials = service_account.Credentials.from_service_account_info(
-        service_account_info,
-        scopes=['https://www.googleapis.com/auth/cloud-platform'],
-    )
+    if os.path.exists(credentials_contents):
+        LOGGER.info('Loading BigQuery credentials from service account file')
+        credentials = service_account.Credentials.from_service_account_file(
+            credentials_contents,
+            scopes=['https://www.googleapis.com/auth/cloud-platform'],
+        )
+    else:
+        LOGGER.info('Loading BigQuery credentials from service account info')
+        service_account_info = json.loads(credentials_contents)
+        credentials = service_account.Credentials.from_service_account_info(
+            service_account_info,
+            scopes=['https://www.googleapis.com/auth/cloud-platform'],
+        )
 
     return bigquery.Client(
         credentials=credentials,
@@ -44,7 +52,14 @@ def run_query(query, dry_run=False, credentials_file=None):
 
     job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
     dry_run_job = client.query(query, job_config=job_config)
-    LOGGER.info('Estimated processed GBs: %.2f', dry_run_job.total_bytes_processed / 1024**3)
+    data_processed_gbs = dry_run_job.total_bytes_processed / 1024**3
+    LOGGER.info('Estimated data processed in query (GBs): %.2f', data_processed_gbs)
+    # https://cloud.google.com/bigquery/pricing#on_demand_pricing
+    # assuming have hit 1 terabyte processed in month
+    cost_per_terabyte = 6.15
+    bytes = dry_run_job.total_bytes_processed
+    cost = cost_per_terabyte * bytes_to_terabytes(bytes)
+    LOGGER.info('Estimated cost for query: $%.2f', cost)
 
     if dry_run:
         return None
@@ -53,5 +68,21 @@ def run_query(query, dry_run=False, credentials_file=None):
     data = query_job.to_dataframe()
     LOGGER.info('Total processed GBs: %.2f', query_job.total_bytes_processed / 1024**3)
     LOGGER.info('Total billed GBs: %.2f', query_job.total_bytes_billed / 1024**3)
-
+    cost = cost_per_terabyte * bytes_to_terabytes(query_job.total_bytes_billed)
+    LOGGER.info('Total cost for query: $%.2f', cost)
     return data
+
+
+def bytes_to_megabytes(bytes):
+    """Convert bytes to megabytes."""
+    return bytes / 1024 / 1024
+
+
+def bytes_to_gigabytes(bytes):
+    """Convert bytes to gigabytes."""
+    return bytes_to_megabytes(bytes) / 1024
+
+
+def bytes_to_terabytes(bytes):
+    """Convert bytes to terabytes."""
+    return bytes_to_gigabytes(bytes) / 1024
